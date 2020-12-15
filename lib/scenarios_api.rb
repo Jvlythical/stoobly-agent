73ab0af7a151b8c35ef8f066f4e759e48cd57375
Michael Yen <lvlichael8@gmail.com>
2020-12-15 01:47:41 -0800

    Add requests_create to ScenariosApi

--- lib/scenarios_api.rb
+++ lib/scenarios_api.rb
@@ -8,6 +8,23 @@ class ScenariosApi
     @api_key = api_key 
   end
 
+  def request_create(project_id, requests, **params)
+    url = "#{@service_url}#{REQUESTS_ENDPOINT}" 
+    uri = URI.parse(url)
+    
+    req = Net::HTTP::Post.new(uri)
+
+    set_headers(req) 
+
+    body = {
+      project_id: project_id,
+      requests: requests,
+    }.merge(params)
+    req.set_form_data(body)
+
+    send(uri, req)
+  end
+
   def request_response(project_id, **query_params)
     url = "#{@service_url}#{REQUESTS_ENDPOINT}/response" 
     uri = URI.parse url
@@ -18,18 +35,27 @@ class ScenariosApi
     uri.query = URI.encode_www_form(params)
 
     req = Net::HTTP::Get.new(uri)
+    set_headers(req)
+    
+    Rails.logger.debug "Requesting: #{uri}"
 
-    headers = default_headers
-    headers.each do |name, value|
-      req[name] = value
-    end
+    send(uri, req) 
+  end
+  
+  private
 
+  def send(uri, req)
     Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
       http.request(req)
     }
   end
-  
-  private
+
+  def set_headers(req)
+    headers = default_headers
+    headers.each do |name, value|
+      req[name] = value
+    end
+  end
 
   def default_headers
     {
