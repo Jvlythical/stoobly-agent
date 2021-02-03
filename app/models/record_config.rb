6a994b60b60a65d86ed22e191e513b7d67f85c32
Michael Yen <Michael Yen>
2021-02-03 00:04:25 -0800

    Add environment and upload_policy enum to record_config model

--- app/models/record_config.rb
+++ app/models/record_config.rb
@@ -1,2 +1,4 @@
 class RecordConfig < ApplicationRecord
+  enum environment: { test: 'test, 'development: 'development', production: 'production'}
+  enum upload_policy: { none: 'none', not_found: 'not_found', any: 'any' }
 end
