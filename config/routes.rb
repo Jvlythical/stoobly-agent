87e332b9926247a52f0774633076f37b7f42a566
Michael Yen <Michael Yen>
2021-02-07 03:11:17 -0800

    Add /record_configs/policies route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/record_configs/policies', to: 'record_configs#policies'
   resources :record_configs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
