307ed5db4ab6ef1d1dd07de420187bad3473ca1d
Michael Yen <Michael Yen>
2021-02-07 03:09:39 -0800

    Update rercord_configs controller

--- app/controllers/record_configs_controller.rb
+++ app/controllers/record_configs_controller.rb
@@ -3,7 +3,9 @@ class RecordConfigsController < ApplicationController
 
   # GET /record_configs
   def index
-    @record_configs = RecordConfig.all
+    @record_configs = begin
+      RecordConfig.all.where(scenarios_project_key: params[:project_key])
+    end
 
     render json: @record_configs
   end
@@ -16,8 +18,13 @@ class RecordConfigsController < ApplicationController
   # POST /record_configs
   def create
     @record_config = RecordConfig.new(record_config_params)
+    @record_config.scenarios_url = RecordConfigFile.instance.scenarios_url
 
     if @record_config.save
+      RecordConfigFile.instance.sync(
+        @record_config.scenarios_project_key, @record_config.scenarios_scenario_key
+      )
+
       render json: @record_config, status: :created, location: @record_config
     else
       render json: @record_config.errors, status: :unprocessable_entity
@@ -27,6 +34,10 @@ class RecordConfigsController < ApplicationController
   # PATCH/PUT /record_configs/1
   def update
     if @record_config.update(record_config_params)
+      RecordConfigFile.instance.sync(
+        @record_config.scenarios_project_key, @record_config.scenarios_scenario_key
+      )
+
       render json: @record_config
     else
       render json: @record_config.errors, status: :unprocessable_entity
@@ -38,6 +49,10 @@ class RecordConfigsController < ApplicationController
     @record_config.destroy
   end
 
+  def policies
+    render json: RecordConfig.scenarios_record_policies.values, status: :ok
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_record_config
@@ -46,6 +61,13 @@ class RecordConfigsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def record_config_params
-      params.fetch(:record_config, {})
+      params.require(:record_config).permit( 
+        :scenarios_api_key, 
+        :scenarios_project_key, 
+        :scenarios_record_policy,
+        :scenarios_record_match_pattern,
+        :scenarios_scenario_key,
+        :service_url
+      )
     end
 end
