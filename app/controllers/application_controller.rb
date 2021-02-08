5bd4dd2365deb592c675f755701f8aa582ff7a5d
Michael Yen <Michael Yen>
2021-02-08 01:25:44 -0800

    Update route logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,55 +8,91 @@ class ApplicationController < ActionController::API
     api = ScenariosApi.new(
       RecordConfigFile.instance.scenarios_url, RecordConfigFile.instance.scenarios_api_key
     )
+
+    modes = RecordConfig.modes
     
-    upload_policy = get_upload_policy
-    record_policies = RecordConfig.scenarios_record_policies
+    mode = RecordConfigFile.instance.mode
+    case mode
+    when modes[:mode_proxy]
+      # 
+      # Try forwarding the request to the service specified by RecordConfigFile.instance.service_url
+      #
+      service_url = get_service_url
+      unless service_url
+        raise 'config/env.yml service_url is not set'
+      end 
 
-    case upload_policy
-    when record_policies[:record_none], record_policies[:record_not_found]
-      res = eval_request(api)
+      disable_web_cache()
 
-      unless res.code == '499'
-        simulate_latency(res['X-RESPONSE-LATENCY'], start_time)
+      options = get_options()
 
-        return pass_on(res)
-      end
+      reverse_proxy service_url, options do |config|
+        config.on_response do |code, res|
+          upload_policy = get_upload_policy
 
-      Rails.logger.warn "Failed to find request, passing on..."
-    end
-    
-    ###
-    #
-    # If either:
-    #   - response was not found
-    #   - config/env.yml upload_policy is not configured
-    # 
-    # then try to forwarding the request to the service specified by RecordConfigFile.instance.service_url
-    #
-    service_url = get_service_url
-    unless service_url
-      raise 'config/env.yml service_url is not set'
-    end 
+          case upload_policy
+          when RECORD_POLICY[:ALL]
+            upload_request(api, res) 
+          when RECORD_POLICY[:NOT_FOUND]
+            res = eval_request(api)
 
-    disable_web_cache()
-
-    options = get_options()
-
-    reverse_proxy service_url, options do |config|
-      config.on_response do |code, res|
-        case upload_policy
-        when record_policies[:record_any] 
-          upload_request(api, res) 
+            upload_request(api, res) if res.code == CUSTOM_RESPONSE_CODES[:NOT_FOUND]
+          when RECORD_POLICY[:NONE]
+            # Do nothing
+          else
+            return bad_request(
+              "Valid env RECORD_POLICY: %s, %s, %s, Got: %s" % 
+              [RECORD_POLICY[:ALL], RECORD_POLICY[:NOT_FOUND], RECORD_POLICY[:NONE], upload_policy]
+            )
+          end
         end
       end
+    when modes[:mode_mock] 
+      mock_policies = RecordConfig.mock_policies
+      mock_policy = get_mock_policy()
+
+      case mock_policy
+      when mock_policies[:mock_none]
+        reverse_proxy service_url, get_options()
+      when mock_policies[:mock_any]
+        res = eval_request(api)
+        simulate_latency(res[CUSTOM_HEADERS[:RESPONSE_LATENCY]], start_time)
+
+        return pass_on(res)
+      when mock_policies[:mock_found]
+        res = eval_request(api)
+
+        if res.code == CUSTOM_RESPONSE_CODES[:NOT_FOUND]
+          reverse_proxy service_url, get_options()
+        else
+          simulate_latency(res[CUSTOM_HEADERS[:RESPONSE_LATENCY]], start_time)
+
+          return pass_on(res)
+        end
+      else
+        return bad_request(
+          "Valid env MOCK_POLICY: %s, %s, %s, Got: %s" % 
+          [MOCK_POLICY[:ALL], MOCK_POLICY[:FOUND], MOCK_POLICY[:NONE], mock_policy]
+        )
+      end
+    else
+      return bad_request(
+        "Valid env MODE: %s, %s, Got: %s" % [MODE[:PROXY], MODE[:MOCK], mode]
+      )
     end
   end
 
   private
 
+  def bad_request(message)
+    render plain: message, status: 400
+  end
+
   def eval_request(api)
     query_params = build_query_params
-    api.request_response(RecordConfigFile.instance.scenarios_project_key, query_params)
+    api.request_response(
+      RecordConfigFile.instance.scenarios_project_key, query_params
+    )
   end
 
   ###
@@ -71,6 +107,7 @@ class ApplicationController < ActionController::API
   #
   def simulate_latency(expected_latency, start_time)
     return if expected_latency.nil?
+
     estimated_rtt_network_latency = 0.015 # seconds
     api_latency = (Time.now - start_time)
     expected_latency = expected_latency.to_f / 1000
@@ -158,11 +195,11 @@ class ApplicationController < ActionController::API
   end
 
   def get_upload_policy
-    request.headers['X-UPLOAD-POLICY'] || RecordConfigFile.instance.scenarios_record_policy
+    request.headers[CUSTOM_HEADERS[:RECORD_POLICY]] || RecordConfigFile.instance.record_policy
   end
 
   def get_service_url
-    request.headers['X-SERVICE-URL'] || RecordConfigFile.instance.service_url
+    request.headers[CUSTOM_HEADERS[:SERVICE_URL]] || RecordConfigFile.instance.service_url
   end
 
   def get_options 
