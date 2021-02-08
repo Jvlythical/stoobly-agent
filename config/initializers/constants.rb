e3c9b99f0304f0fcf015fbd741a32f4b5494837d
Michael Yen <Michael Yen>
2021-02-08 01:24:44 -0800

    Update constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,26 @@
-UPLOAD_POLICY = {
-  ANY: 'any',
+MOCK_POLICY = {
+  ALL: 'all',
   NONE: 'none',
-  NOT_FOUND: 'not_found',
+  FOUND: 'found',
+}
+
+RECORD_POLICY = {
+  NONE: 'record_none',
+  ALL: 'record_all',
+  NOT_FOUND: 'record_not_found',
+}
+
+CUSTOM_RESPONSE_CODES = {
+  NOT_FOUND: '499'
+}
+
+MODE = {
+  MOCK: 'mock',
+  PROXY: 'proxy',
+}
+
+CUSTOM_HEADERS = {
+  RECORD_POLICY:'X-UPLOAD-POLICY', 
+  RESPONSE_LATENCY: 'X-RESPONSE-LATENCY',
+  SERVICE_URL: 'X-SERVICE-URL',
 }
