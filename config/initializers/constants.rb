de92c085f8c9d2f2d86062693522d771dd911903
Michael Yen <Michael Yen>
2021-03-01 18:31:40 -0800

    Add COMPONENT_TYPES to constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,27 +1,36 @@
+COMPONENT_TYPES = {
+  HEADER: 1,
+  PATH_SEGMENT: 2,
+  QUERY_PARAM: 3,
+  BODY_PARAM: 4,
+  RESPONSE: 5
+}.freeze
+
 MOCK_POLICY = {
   ALL: 'all',
   NONE: 'none',
   FOUND: 'found',
-}
+}.freeze
 
 RECORD_POLICY = {
   NONE: 'none',
   ALL: 'all',
   NOT_FOUND: 'not_found',
-}
+}.freeze
 
 MODE = {
   MOCK: 'mock',
   RECORD: 'record',
-}
+}.freeze
 
 CUSTOM_RESPONSE_CODES = {
-  NOT_FOUND: '499'
-}
+  NOT_FOUND: '499',
+  IGNORE_COMPONENTS: '498',
+}.freeze
 
 CUSTOM_HEADERS = {
   RECORD_POLICY: 'X-RECORD-POLICY', 
   RESPONSE_LATENCY: 'X-RESPONSE-LATENCY',
   SERVICE_URL: 'X-SERVICE-URL',
   MOCK_POLICY: 'X-MOCK-POLICY', 
-}
+}.freeze
